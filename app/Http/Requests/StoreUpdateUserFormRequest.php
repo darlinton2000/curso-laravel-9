9c5d7e7a73145e2d2273ee8a74f42283f5bfc52e
root <root@DARLINTON-PC.localdomain>
2023-02-07 14:07:16 -0300

    Função para editar e validar os usuários criados

--- app/Http/Requests/StoreUpdateUserFormRequest.php
+++ app/Http/Requests/StoreUpdateUserFormRequest.php
@@ -23,10 +23,31 @@ class StoreUpdateUserFormRequest extends FormRequest
      */
     public function rules()
     {
-        return [
+        $id = $this->id ?? '';
+
+        $rules = [
             'name' => 'required|string|max:255|min:3',
-            'email' => 'required|email|unique:users',
-            'password' => 'required|max:15|min:6'
+            'email' => [
+                'required',
+                'email',
+                "unique:users,email,{$id},id",
+            ],
+            'password' => [
+                'required',
+                'min:6',
+                'max:15',
+            ]
         ];
+
+        if ($this->method('PUT')){
+            $rules['password'] = [
+                'nullable',
+                'min:6',
+                'max:15',
+            ];
+            
+        }
+
+        return $rules;
     }
 }
--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -16,8 +16,8 @@
     <form action="{{ route('users.store') }}" method="post">
         @csrf
 
-        <input type="text" name="name" placeholder="Nome:" value="{{ old('name')}}">
-        <input type="email" name="email" placeholder="E-mail:" value="{{ old('email')}}">
+        <input type="text" name="name" placeholder="Nome:" value="{{ old('name') }}">
+        <input type="email" name="email" placeholder="E-mail:" value="{{ old('email') }}">
         <input type="password" name="password" placeholder="Senha:">
 
         <button type="submit">Enviar</button>
