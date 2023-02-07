a06809f5a867f29f099f3907f4e7747ed9f8bb54
root <root@DARLINTON-PC.localdomain>
2023-02-07 10:28:47 -0300

    Template básico criado

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -1,6 +1,12 @@
-<h1>Listagem do Usuário {{ $user->name }}</h1>
+@extends('layouts.app')
 
-<ul>
-    <li>{{ $user->name }}</li>
-    <li>{{ $user->email }}</li>
-</ul>
+@section('title', 'Listagem do Usuário')
+
+@section('content')
+    <h1>Listagem do Usuário {{ $user->name }}</h1>
+
+    <ul>
+        <li>{{ $user->name }}</li>
+        <li>{{ $user->email }}</li>
+    </ul>
+@endsection
