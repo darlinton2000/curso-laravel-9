fe2c41e777fdcc53aad47bc8b22f3c8daca607dd
root <root@DARLINTON-PC.localdomain>
2023-02-07 12:12:36 -0300

    Formulário novo usuário + validação criados

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreUpdateUserFormRequest;
 use App\Models\User;
 use Illuminate\Http\Request;
 
@@ -22,5 +23,27 @@ class UserController extends Controller
         }
 
         return view('users.show', compact('user'));
+    }
+
+    public function create()
+    {
+        return view('users.create');
+    }
+
+    public function store(StoreUpdateUserFormRequest $request)
+    {
+        $data = $request->all();
+        $data['password'] = bcrypt($request->password);
+
+        $user = User::create($data);
+
+        //return redirect()->route('users.show', $user->id);
+        return redirect()->route('users.index');
+        
+        /* $user = new User();
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->password = $request->password;
+        $user->save(); */
     }
 }
