a267d60ef1689250fe4131e004081f48adea23e1
root <root@DARLINTON-PC.localdomain>
2023-02-07 14:19:15 -0300

    Função deletar o usuário criada

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -71,4 +71,15 @@ class UserController extends Controller
 
         return redirect()->route('users.index');
     }
+
+    public function delete($id)
+    {
+        if (!$user = User::find($id)){
+            return redirect()->route('users.index');
+        }
+
+        $user->delete();
+
+        return redirect()->route('users.index');
+    }
 }
