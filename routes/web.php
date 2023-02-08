acbd6eccad1a19de3849cb509913bf924debc623
root <root@DARLINTON-PC.localdomain>
2023-02-08 15:08:42 -0300

    Função listar as anotações/comentários dos usuários criada

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Admin\CommentController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
@@ -17,6 +18,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/users/{id}/comments', [CommentController::class, 'index'])->name('comments.index');
 
 Route::delete('/users/{id}', [UserController::class, 'delete'])->name('users.delete');
 Route::put('/users/{id}', [UserController::class, 'update'])->name('users.update');
